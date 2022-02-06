new shiny multiprocessing code, fixing unit things (comoving, physical, Mpc/h vs Mpc in class output), cosmology, redshift weighting now correct, bootstrap errors and combining BN and D56

hydro_tests/define_galaxies.py
@@ -5,17 +5,23 @@ import plotparams
 plotparams.buba()
 
 fp_dm = 'fp'
-#data_dir = "/mnt/alan1/boryanah/MTNG/data_fp/"; pos_unit = 1.; Lbox = 500. Mpc/h
-data_dir = "/mnt/gosling1/boryanah/TNG300/"; pos_unit = 1.e3; Lbox = 205.
-#snapshot = 264
-snapshot = 99
+data_dir = "/mnt/alan1/boryanah/MTNG/data_fp/"; pos_unit = 1.; Lbox = 500. # cMpc/h
+#data_dir = "/mnt/gosling1/boryanah/TNG300/"; pos_unit = 1.e3; Lbox = 205. # cMpc/h
+snapshot = 264; z = 0.
+#snapshot = 214; z = 0.5
+#snapshot = 237; z = 0.245
+#snapshot = 99
 if snapshot == 99:
     snap_str = ""
 else:    
     snap_str = f"_{snapshot:d}"
-    
+h = 0.6744
+
 # load subhalo fields at z = 0
-SubhaloGrNr = np.load(f"{data_dir:s}/SubhaloGrNr_{fp_dm:s}{snap_str:s}.npy")
+if 'MTNG' in data_dir:
+    SubhaloGrNr = np.load(f"{data_dir:s}/SubhaloGroupNr_{fp_dm:s}{snap_str:s}.npy")
+else:
+    SubhaloGrNr = np.load(f"{data_dir:s}/SubhaloGrNr_{fp_dm:s}{snap_str:s}.npy")
 SubhaloStellarPhotometrics = np.load(f"{data_dir:s}/SubhaloStellarPhotometrics_{fp_dm:s}{snap_str:s}.npy") #U, B, V, K, g, r, i, z [mag]
 SubhaloMstar = np.load(f"{data_dir:s}/SubhaloMassType_{fp_dm:s}{snap_str:s}.npy")[:, 4]*1.e10
 SubhaloSFR = np.load(f"{data_dir:s}/SubhaloSFR_{fp_dm:s}{snap_str:s}.npy")
@@ -24,9 +30,10 @@ SubhalosSFR[SubhaloMstar == 0.] = 0.
 
 # load halo fields
 Group_M_TopHat200 = np.load(f"{data_dir:s}/Group_M_TopHat200_{fp_dm:s}{snap_str:s}.npy")*1.e10
+Group_R_TopHat200 = np.load(f"{data_dir:s}/Group_R_TopHat200_{fp_dm:s}{snap_str:s}.npy")/pos_unit # comoving Mpc/h
 
 # how many galaxies
-ngal = 1.e-2 # gal/(Mpc/h)^3
+ngal = 1.e-4 # gal/(Mpc/h)^3
 Ngal = int(ngal*Lbox**3)
 print("number of galaxies = ", Ngal)
 print("number density of galaxies = ", ngal)
@@ -48,6 +55,19 @@ par_mass = Group_M_TopHat200[par_inds]
 par_mass_cent = Group_M_TopHat200[par_inds_cent]
 gal_mass = SubhaloMstar[gal_inds]
 gal_ssfr = SubhalosSFR[gal_inds]
+
+par_r200 = Group_R_TopHat200[gal_inds]
+par_r200 /= h #cMpc
+print("redshift = ", z )
+print("median and mean r200 [cMpc] = ", np.median(par_r200), np.mean(par_r200)) # 0.068, 0.082
+print("median and mean r200 [Mpc] = ", np.median(par_r200)*1./(1+z), np.mean(par_r200)*1./(1+z)) 
+rbins = np.geomspace(0.01, par_r200.max(), 51)
+rbinc = (rbins[1:] + rbins[:-1]) * .5
+hist, _ = np.histogram(par_r200, bins=rbins)
+plt.plot(rbinc, hist)
+plt.xscale("log")
+plt.xlabel("Comoving radius [Mpc]")
+plt.show()
 
 # mass bins
 mbins = np.logspace(11, 15, 41)

pairwise/estimator.py
@@ -14,7 +14,6 @@ def pairwise_momentum(X, dT, bins, is_log_bin, dtype=np.float32, nthread=1):
     V_los is independently measured
     Does not use periodic boundary conditions
     """
-    # TODO: check that the pairwise velocity thingy works
 
     # multithreading
     numba.set_num_threads(nthread)
@@ -73,8 +72,7 @@ def pairwise_momentum(X, dT, bins, is_log_bin, dtype=np.float32, nthread=1):
             dx = x1-x2
             dy = y1-y2
             dz = z1-z2
-            dist2 = dx**two + dy**two + dz**two
-            dist = np.sqrt(dist2)
+            dist = np.sqrt(dx**two + dy**two + dz**two)
 
             # index where this pair belongs
             if is_log_bin:

pairwise/get_tSZ.py
@@ -17,23 +17,19 @@ from estimator import pairwise_momentum
 # settings
 #rApMinArcmin = 1. # minimum aperture for bins
 rApMaxArcmin = 10. # maximum aperture for bins
-#resCutoutArcmin = 0.5 # stamp resolution
 resCutoutArcmin = 0.25 # stamp resolution
 projCutout = 'cea' # projection
 want_jackknife = False
 h = 0.7; Omega_Lambda = 0.7; Omega_cdm = 0.25; Omega_b = 0.05
 
 # galaxy and cmb sample
-cmb_sample = "ACT_BN"
-#cmb_sample = "ACT_D56"
+#cmb_sample = "ACT_BN"
+cmb_sample = "ACT_D56"
 #galaxy_sample = "BOSS_South" # goes with D56
-#galaxy_sample = "BOSS_North" # goes with BN # slow
-#galaxy_sample = "2MPZ" # both ACT maps
-#galaxy_sample = "SDSS_L43D"
-#galaxy_sample = "SDSS_L61D"
-#galaxy_sample = "SDSS_L43"
-galaxy_sample = "SDSS_L61"
-#galaxy_sample = "SDSS_L79"
+#galaxy_sample = "BOSS_North" # goes with BN
+galaxy_sample = "2MPZ" # both ACT maps
+#galaxy_sample = "SDSS_L43D" # "SDSS_L61D" # "SDSS_L43" # "SDSS_L61" # "SDSS_L79"
+#galaxy_sample = "SDSS_L61"
 print(f"Producing: {galaxy_sample:s}_{cmb_sample:s}")
 
 # filename of galaxy map
@@ -47,26 +43,28 @@ elif "SDSS" in galaxy_sample:
     gal_fn = "../galaxy_tests/sdss_data/V21_DR15_Catalog_v4.txt"
     if "L43" in galaxy_sample:
         L_lo = 4.3e10
-        if "D" in galaxy_sample:
+        if "L43D" in galaxy_sample:
             L_hi = 6.1e10
         else:
             L_hi = 1.e20
     elif "L61" in galaxy_sample:
         L_lo = 6.1e10
-        if "D" in galaxy_sample:
+        if "L61D" in galaxy_sample:
             L_hi = 7.9e10
         else:
             L_hi = 1.e20
     elif "L79" in galaxy_sample:
         L_lo = 7.9e10
-    
+        L_hi = 1.e20
+
 # filename of CMB map
 if cmb_sample == "ACT_BN":
     #fn = "../cmb_tests/cmb_data/tilec_single_tile_BN_cmb_map_v1.2.0_joint.fits" # BN
     fn = "../cmb_tests/cmb_data/tilec_single_tile_BN_comptony_map_v1.2.0_joint.fits" # BN
     msk_fn = "../cmb_tests/cmb_data/act_dr4.01_s14s15_BN_compsep_mask.fits"
 elif cmb_sample == "ACT_D56":
-    fn = "../cmb_tests/cmb_data/tilec_single_tile_D56_cmb_map_v1.2.0_joint.fits" # D56
+    #fn = "../cmb_tests/cmb_data/tilec_single_tile_D56_cmb_map_v1.2.0_joint.fits" # D56
+    fn = "../cmb_tests/cmb_data/tilec_single_tile_D56_comptony_map_v1.2.0_joint.fits" # D56
     msk_fn = "../cmb_tests/cmb_data/act_dr4.01_s14s15_D56_compsep_mask.fits"
     
 # reading fits file
@@ -76,12 +74,14 @@ if cmb_sample == "ACT_BN":
     pass
 elif cmb_sample == "ACT_D56":
     pass
-    
+print("percentage not masked = ", np.sum(msk)*100./(msk.shape[0]*msk.shape[1])) # 58%
+
+
 # save map
 save = 0
 if save:
     fig_name = (fn.split('/')[-1]).split('.fits')[0]
-    #eshow(mp, fig_name, **{"colorbar":True, "range": 300, "ticks": 5, "downgrade": 4})
+    eshow(mp, fig_name, **{"colorbar":True, "range": 300, "ticks": 5, "downgrade": 4})
     eshow(msk, fig_name+"_mask", **{"colorbar":True, "ticks": 5, "downgrade": 4})
     plt.close()
 
@@ -104,14 +104,15 @@ print("decfrom, decto, rafrom, rato = ", decfrom, decto, rafrom, rato)
 
 # size of the clusters and median z of 2MPZ
 if galaxy_sample == '2MPZ':
-    goal_size = 1.5*h # Mpc/h
+    goal_size = 0.5#1.5 # cMpc
     zmed = 0.08
 elif 'BOSS' in galaxy_sample:
-    goal_size = 1.1*h # Mpc/h
+    goal_size = 1.1 # cMpc
     zmed = 0.5
 elif 'SDSS' in galaxy_sample:
-    goal_size = 1.1*h # Mpc/h
+    goal_size = 1.1 # cMpc
     zmed = 0.5
+goal_size *= 1./(1+zmed) # Mpc
 sigma_z = 0.01
 
 # create instance of the class "Class"
@@ -121,7 +122,7 @@ Cosmo.set(param_dict)
 Cosmo.compute()
 
 # compute angular size distance at median z of 2MPZ
-D_A_zmed = Cosmo.luminosity_distance(zmed)/(1.+zmed)**2 # Mpc/h
+D_A_zmed = Cosmo.luminosity_distance(zmed)/(1.+zmed)**2 # Mpc
 theta_arcmin_zmed = goal_size/D_A_zmed / utils.arcmin
 print("theta_arcmin_zmed = ", theta_arcmin_zmed)
 
@@ -146,7 +147,7 @@ elif 'SDSS' in galaxy_sample:
     DEC = data['dec'] # -180, 180 # -10, 36
     Z = data['z']
     L = data['lum']
-    choice = (L_lo < L) & (L_hi >= L) 
+    choice = (L > L_lo) & (L <= L_hi) 
     #choice = np.ones(len(Z), dtype=bool)
 print("Zmin/max/med = ", Z.min(), Z.max(), np.median(Z))
 # transform to cartesian coordinates (checked)
@@ -172,22 +173,26 @@ stack = 0
 count = 0
 for i in range(len(RA)):
     if i % 1000 == 0: print("i = ", i)
-    if i == 10000: break
+    #if i == 10000: break
     ra = RA[i]
     dec = DEC[i]
+    
     # Extract stamps by reprojecting to a tangent plane projection
-    if ra > 180.: ra -= 360. # option 0
+    if ra > 180.: ra -= 360.
 
     # extract stamp
     opos, stampMap, stampMask = extractStamp(mp, ra, dec, rApMaxArcmin=rApMaxArcmin, resCutoutArcmin=resCutoutArcmin, projCutout=projCutout, pathTestFig='figs/', test=False, cmbMask=msk)
-    if np.sum(stampMask) == 0.: continue
+    if (np.sum(stampMask) == 0.): continue
+    if (stampMap is None): print("None, ra, dec = ", ra, dec); continue
+    
     stack += stampMap
     count += 1
 stack /= count
-print(count)
+print("nonzero objects percentage = ", count*100./np.sum(choice))
 plt.imshow(stack)
-plt.savefig(f"figs/{galaxy_sample}_{cmb_sample:s}_tSZ_stack.png")
-plt.show()
+#plt.savefig(f"figs/{galaxy_sample}_{cmb_sample:s}_tSZ_stack.png")
+#plt.show()
+plt.close()
 
 plots = enplot.plot(enmap.upgrade(stack, 5), grid=True)
-enplot.write("figs/tSZ", plots)
+enplot.write(f"figs/tSZ_{galaxy_sample}_{cmb_sample:s}", plots)

pairwise/util.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import numba
+from scipy import interpolate
 
 from pixell import enmap, reproject, enplot, curvedsky, utils
 
@@ -60,7 +62,7 @@ def extractStamp(cmbMap, ra, dec, rApMaxArcmin, resCutoutArcmin, projCutout, pat
         stampMask[:, :] = cmbMask.at(ipos, prefilter=True, mask_nan=False, order=1)
     
         # re-threshold the mask map, to keep 0 and 1 only
-        stampMask[:, :] = 1.*(stampMask[:,:]>0.5)
+        stampMask[:, :] = 1.*(stampMask[:, :]>0.5)
     
     if test:
         print("Extracted cutouts around ra=", ra, "dec=", dec)
@@ -74,6 +76,52 @@ def extractStamp(cmbMap, ra, dec, rApMaxArcmin, resCutoutArcmin, projCutout, pat
     if cmbMask is not None:
         return opos, stampMap, stampMask
     return opos, stampMap
+
+@numba.guvectorize(['float64[:],float64[:],float64[:],float64[:],'
+                    'float64[:],float64[:]'],
+                   '(n),(n),(),()->(),()', target='parallel')
+def get_tzav_and_w_nb(dT, z, zj, sigma_z, res1, res2):
+    '''Launched by get_tzav to compute formula in parallel '''
+    for i in range(dT.shape[0]):
+        res1 += dT[i] * np.exp(-(zj[0]-z[i])**2.0/(2.0*sigma_z**2))
+        res2 += np.exp(-(zj[0]-z[i])**2/(2.0*sigma_z**2))
+
+def get_tzav(dTs, zs, sigma_z):
+    '''Computes the dT dependency to redshift.
+    dTs: Temperature decrements
+    zs: redshifts
+    sigma_z: width of the gaussian to smooth out the moving window.'''
+    #To test, run test_get_tzav_nb
+    #   Create empty arrays to be used by numba in get_tzav_and_w_nb'''
+    res1 = np.zeros(dTs.shape[0])
+    res2 = np.zeros(dTs.shape[0])
+    get_tzav_and_w_nb(dTs, zs, zs, sigma_z, res1, res2)
+    return res1/res2
+
+
+def get_tzav_fast(dTs, zs, sigma_z):
+    '''Subsample and interpolate Tzav to make it fast.
+    dTs: entire list of dT decrements
+    zs: entire list of redshifts
+    sigma_z: width of the gaussian kernel we want to apply.
+    '''
+    N_samples_in_sigmaz = 15  # in one width of sigmaz use Nsamples
+    zmin, zmax = zs.min(), zs.max()
+    delta_z = zmax - zmin
+
+    # evaluates Tzav N times
+    N_samples = int(round(delta_z/sigma_z)) * N_samples_in_sigmaz
+    z_subsampled = np.linspace(zmin, zmax, N_samples)
+
+    #now compute tzav as we usually do.
+    res1 = np.zeros(z_subsampled.shape[0])
+    res2 = np.zeros(z_subsampled.shape[0])
+    get_tzav_and_w_nb(dTs, zs, z_subsampled, sigma_z, res1, res2)
+    tzav_subsampled = res1/res2
+    #interpolate
+    f = interpolate.interp1d(z_subsampled, tzav_subsampled, kind='cubic')
+    tzav_fast = f(zs)
+    return tzav_fast
 
 def calc_T_AP(imap, rad_arcmin, test=False, mask=None):
     modrmap = imap.modrmap()